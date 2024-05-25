1ec98010a7458dff2c5b30b1b7b8f932aad62829
Moses Mwai <mosesmwaiw@gmail.com>
2024-05-25 10:47:48 +0300

    Update content

---
+++ home/index.js
@@ -0,0 +1,29 @@
+let initSlider = ()=>{
+    const imageList = document.querySelector(".illustration .illustration__carousel");
+    const slideButtons = document.querySelectorAll(".carousel-control");
+    const maxScrollLeft = imageList.scrollWidth ;
+
+    slideButtons.forEach(button=>{
+        button.addEventListener("click", ()=>{
+            const direction = button.id=="prev-btn"? -1: 1;
+            const scrollAmount = imageList.clientWidth * direction
+
+            imageList.scrollBy({
+                left: scrollAmount,
+                behavior: 'smooth'
+            });
+
+        })
+    })
+
+    const handleSlideBtns = ()=>{
+        slideButtons[0].style.display = imageList.scrollLeft <=0 ? "none": "block";
+        slideButtons[1].style.display = imageList.scrollLeft >= maxScrollLeft ? "none":"block";
+    }
+
+    imageList.addEventListener("scroll", ()=>{
+        handleSlideBtns()
+    })
+}
+
+window.addEventListener("DOMContentLoaded", initSlider)
